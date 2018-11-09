0fc6b579ecda0ea4783c5f180db06f9aac7b0f36
kashwa <kashwa80@gmail.com>
2018-11-09 19:52:02 +0200

    style indexing users

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -8,17 +8,24 @@
         <div class="card">
           <div class="card-header">Employees</div>
             @foreach ($employees as $emp)
-            <div class="card-body">
-              <strong>Name: </strong> {{$emp->name}} <br>
-              <strong>Email: </strong> {{$emp->email}} <br>
-              <strong>Hired at: </strong> {{$emp->hire_date}} <br>
-              <strong>Phone: </strong> {{$emp->mobile_no}} <br>
-              <form id="DeleteBtn" action="{{ route('employees.destroy', ['id' => $emp->id]) }}" method="POST">
-                <input type="submit" value="delete" style="background-color: #f2134b; color: white" class="button is-outlined" onclick="return confirm('Are you sure, You want to delete Post?')">  {{ method_field('DELETE') }} {!! csrf_field() !!}
-                <br>
-                <a href="{{route('employees.edit', $emp->id)}}">Edit</a>
-              </form>
-              <hr>
+            <div class="row">
+              <div class="col">
+                <div class="card-body">
+                  <strong>Name: </strong> {{$emp->name}} <br>
+                  <strong>Email: </strong> {{$emp->email}} <br>
+                  <strong>Hired at: </strong> {{$emp->hire_date}} <br>
+                  <strong>Phone: </strong> {{$emp->mobile_no}} <br>
+                </div>
+              </div>
+              <div class="col">
+                <div class="container" style="margin-top: 20px;">
+                  <form id="DeleteBtn" action="{{ route('employees.destroy', ['id' => $emp->id]) }}" method="POST">
+                    <input type="submit" value="delete" style="background-color: #f2134b; color: white" class="button is-outlined" onclick="return confirm('Are you sure, You want to delete Post?')">      {{ method_field('DELETE') }} {!! csrf_field() !!}
+              
+                    <a style="margin-left: 5%" class="btn btn-primary" href="{{route('employees.edit', $emp->id)}}">Edit</a>
+                  </form>
+                </div>
+              </div>
             </div>
             @endforeach
             <div class="container" style="margin: 20px; padding-left:15px">
